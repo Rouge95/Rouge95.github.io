bdfd316fad6322ff0469bbc8d3474f80214f7be3
mide73924 <micheldeosaran@proton.me>
2026-01-08 16:53:32 -0500

    Initial commit: add .astro, css, js, and assets

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://mide73924.github.io",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
